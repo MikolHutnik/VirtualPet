debb59a72842b05cbc195291904588d352084d6e
MikolHutnik <mikolhutnik@gmail.com>
2018-06-02 13:25:18 -0400

    dragon

--- MikolHutnikProject3/Pet.cs
+++ MikolHutnikProject3/Pet.cs
@@ -8,5 +8,169 @@ namespace MikolHutnikProject3
 {
     class Pet
     {
-    }
-}
+        // fields
+        private int hunger;
+        private int thirst;
+        private int waste;
+        private bool hungerAlert;
+        private bool thirstAlert;
+        private bool wasteAlert;
+        private string name;
+
+        // properties
+        public bool HungerAlert
+        {
+            get { return this.hungerAlert; }
+            set { this.hungerAlert = value; }
+        }
+
+        public bool ThirstAlert
+        {
+            get { return this.thirstAlert; }
+            set { this.thirstAlert = value; }
+        }
+
+        public bool WasteAlert
+        {
+            get { return this.wasteAlert; }
+            set { this.wasteAlert = value; }
+        }
+
+        public int Hunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
+        }
+
+        public int Thirst
+        {
+            get { return this.thirst; }
+            set { this.thirst = value; }
+        }
+
+        public int Waste
+        {
+            get { return this.waste; }
+            set { this.waste = value; }
+        }
+        
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        
+        //default constructor
+        public Pet()
+        {
+            //default constructor
+        }
+
+        public Pet(string name, int hunger, int thirst, int waste)
+        {
+            this.Name = name;
+            this.Hunger = hunger;
+            this.Thirst = thirst;
+            this.Waste = waste;
+        }
+
+        // Tick 
+        public void Tick()
+        {
+            Hunger++;
+            if (Hunger > 35)
+            {
+                StarveAlert(true);
+            }
+            else
+            {
+                StarveAlert(false);
+            }
+
+            Thirst++;
+            if (Thirst > 35)
+            {
+                DehydrationAlert(true);
+            }
+            else
+            {
+                DehydrationAlert(false);
+            }
+
+            Waste++;
+            if (Waste > 35)
+            {
+                PooAlert(true);
+            }
+            else
+            {
+               PooAlert(false);
+            }
+        }//tick end
+
+        public bool StarveAlert(bool status)
+        {
+            HungerAlert = status;
+            return HungerAlert;
+        }
+
+        public bool DehydrationAlert(bool status)
+        {
+            ThirstAlert = status;
+            return ThirstAlert;
+        }
+
+        public bool PooAlert(bool status)
+        {
+            WasteAlert = status;
+            return WasteAlert;
+        }
+
+        public int SetHunger(int num)
+        {
+            this.Hunger = num;
+            return Hunger;
+        }
+        public int SetThirst(int num)
+        {
+            this.Thirst = num;
+            return Thirst;
+        }
+
+        public int SetWaste(int num)
+        {
+            this.Waste = num;
+            return Waste;
+        }
+
+        public string SetName(string name)
+        {
+            this.Name = name;
+            return Name;
+        }
+
+        public void DragonPic()
+        {
+
+            Console.WriteLine(@"//                     ___====-_  _-====___                        //");
+            Console.WriteLine(@"//              __--^^^      //     \\     ^^^--_                  //");
+            Console.WriteLine(@"//             _-^         // (    ) \\         ^-_                //");
+            Console.WriteLine(@"//            -           //  |\^^/|  \\           -               //");
+            Console.WriteLine(@"//          _/           //   (0::0)   \\            \_            //");
+            Console.WriteLine(@"//         /            ((     \\//     ))             \           //");
+            Console.WriteLine(@"//       -               \\    (oo)    //               -          //");
+            Console.WriteLine(@"//      -                 \\  / \/ \  //                 -         //");
+            Console.WriteLine(@"//     -                   \\/      \//                   -        //");
+            Console.WriteLine(@"//   / /|           /\      (        )      /\           |\ \      //");
+            Console.WriteLine(@"//   |/ | /\_/\_/\_/  \_/\  (   /\   )  /\_/  \_/\_/\_/\ | \|      //");
+            Console.WriteLine(@"//   `  |/  V  V  `    V  \_(| |  | |)_/  V    '  V  V  \|  '      //");
+            Console.WriteLine(@"//      `   `  `       `   / | |  | | \   '       '  '   '         //");
+            Console.WriteLine(@"//                       <(  | |  | |  )>                          //");
+            Console.WriteLine(@"//                      <__\_| |  | |_\__>                         //");
+            Console.WriteLine(@"//                      ^^^^ ^^^  ^^^ ^^^^^                        //");
+
+        }
+
+
+    }//class end
+}//namespace end
--- MikolHutnikProject3/Program.cs
+++ MikolHutnikProject3/Program.cs
@@ -10,6 +10,87 @@ namespace MikolHutnikProject3
     {
         static void Main(string[] args)
         {
+
+            Pet Dragon = new Pet("Dragon", 26, 26, 26);
+            Dragon.DragonPic();
+            Console.WriteLine("Welcome to the Virtual Pet Program");
+            Console.WriteLine("Please type Yes to manage your {0}!", Dragon.Name);
+            string userResponse = Console.ReadLine().ToLower();
+            while (userResponse.Equals("yes"))
+            {
+                Dragon.DragonPic();
+                Console.WriteLine("Please select from the following to manage your {0}.", Dragon.Name);
+
+                Console.WriteLine("Select 1 to get the status of your {0}.", Dragon.Name);
+                Console.WriteLine("Select 2 to feed your {0}.", Dragon.Name);
+                Console.WriteLine("Select 3 to water the {0}.", Dragon.Name);
+                Console.WriteLine("Select 4 to walk your {0}.", Dragon.Name);
+                int userChoice = int.Parse(Console.ReadLine());
+
+                switch (userChoice)
+                {
+                    case 1:
+                        Dragon.Tick();
+                        if (Dragon.HungerAlert)
+                        {
+                            Console.WriteLine("The {0} is hungry and needs to be fed.", Dragon.Name);
+                            Console.WriteLine("Please enter a new food amount for the {0}.", Dragon.Name);
+                            int hungerAlert = int.Parse(Console.ReadLine());
+                            Dragon.SetHunger(hungerAlert);
+
+                        }
+                        if (Dragon.ThirstAlert)
+                        {
+                            Console.WriteLine("The {0}'s thirst levels are high and he needs water.", Dragon.Name);
+                            Console.WriteLine("Please enter a new water level for the {0}.", Dragon.Name);
+                            int thirstAlert = int.Parse(Console.ReadLine());
+                            Dragon.SetThirst(thirstAlert);
+
+                        }
+                        if (Dragon.WasteAlert)
+                        {
+                            Console.WriteLine("The {0} is {1} deep and needs to be fed.", Dragon.Name, Dragon.WasteAlert);
+                            Console.WriteLine("Please enter a new food amount for the {0}.", Dragon.Name);
+                            int wasteAlert = int.Parse(Console.ReadLine());
+                            Dragon.SetWaste(wasteAlert);
+
+                        }
+                        Console.WriteLine("The {0}'s levels for Hunger: {1} Thirst: {2} Waste {3}", Dragon.Name, Dragon.Hunger, Dragon.Thirst, Dragon.Waste);
+
+                        break;
+                    case 2:
+                        Dragon.Tick();
+                        Console.WriteLine("Enter a new food amount.");
+                        int hunger = int.Parse(Console.ReadLine());
+                        Dragon.SetHunger(hunger);
+                        Console.WriteLine("The new food amount for the {0} is {1}", Dragon.Name, Dragon.Hunger);
+                        break;
+                    case 3:
+                        Dragon.Tick();
+                        Console.WriteLine("Enter a new water amount level.");
+                        int thirst = int.Parse(Console.ReadLine());
+                        Dragon.SetThirst(thirst);
+                        Console.WriteLine("The new thirst level of the {0} is {1}", Dragon.Name, Dragon.Thirst);
+                        break;
+                    case 4:
+                        Dragon.Tick();
+                        Console.WriteLine("Enter a new amount of time outside.");
+                        int waste = int.Parse(Console.ReadLine());
+                        Dragon.SetWaste(waste);
+                        Console.WriteLine("The new poo level of the {0} is {1}", Dragon.Name, Dragon.Waste);
+                        break;
+                    default:
+                        break;
+
+                }
+                Dragon.Tick();
+                Console.WriteLine("Please type Yes to manage your {0}!", Dragon.Name);
+                userResponse = Console.ReadLine().ToLower();
+            }
+
+
+
+
         }
     }
 }
